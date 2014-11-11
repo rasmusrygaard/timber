bdb97ac3dab1e447a0b3ccc009170f68e2757bc0
Rasmus Rygaard <rasmus@rasmusrygaard.com>
2014-11-10 21:47:31 -0800

    Smarter init script.

--- logcabin/init.sh
+++ logcabin/init.sh
@@ -1,13 +1,18 @@
-git clone https://github.com/logcabin/logcabin.git
-cd logcabin
-git submodule update --init
-sudo apt-get install -y scons protobuf-compiler libcrypto++
-wget https://protobuf.googlecode.com/files/protobuf-2.5.0.tar.bz2
-tar -xvjf protobuf-2.5.0.tar.bz2
-cd protobuf-2.5.0.tar.bz2
-./configure --prefix=/usr
-make
-make check
-sudo make install
-cd ..
-scons
+cd ~
+if ! which protoc >/dev/null; then
+  wget https://protobuf.googlecode.com/files/protobuf-2.5.0.tar.bz2
+  tar -xvjf protobuf-2.5.0.tar.bz2
+  cd protobuf-2.5.0
+  ./configure --prefix=/usr
+  make
+  make check
+  sudo make install
+fi
+cd ~
+if [ -d "logcabin" ]; then
+  git clone https://github.com/logcabin/logcabin.git
+  cd logcabin
+  git submodule update --init
+  sudo apt-get install -y scons protobuf-compiler libcrypto++
+  scons
+fi
