baee306a11cb72ccd206edbedf69c873b71cae69
Rasmus Rygaard <rasmus@rasmusrygaard.com>
2014-11-18 12:14:36 -0800

    Pull if timber is already cloned.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,13 @@
 sudo apt-get -y update
 sudo apt-get install -y git build-essential emacs curl wget iptables emacs automake pkg-config
-git clone https://github.com/rasmusrygaard/timber.git
+if [ ! -d 'timber' ]
+then
+    git clone https://github.com/rasmusrygaard/timber.git
+else
+    cd timber
+    git pull origin master
+    cd -
+fi
 iptables -A INPUT -p tcp --dport 6160 -j ACCEPT
 cd timber
 make
