5b789c8d2dfa4c1c36dbde5f5f36b36871996e6f
Rasmus Rygaard <rasmus@rasmusrygaard.com>
2014-11-21 10:08:13 -0800

    Better logging.

--- server/LogCabinWrapper.cc
+++ server/LogCabinWrapper.cc
@@ -14,37 +14,39 @@ LogCabinWrapper::initialize(const std::vector<std::string>& hosts) {
     writeConfig(hosts);
     std::cout << "Running init.sh for LogCabin" << std::endl;
     system(("bash " + LOGCABIN_SCRIPTS_DIR + "init.sh").c_str());
+    std::cout << "Done running init.sh for LogCabin" << std::endl;
 }
 
 void
 LogCabinWrapper::writeConfig(const std::vector<std::string>& hosts) {
-  std::cout << "Writing logcabin.conf with hosts " << joinHosts(hosts, ";") << std::endl;
+    std::cout << "Writing logcabin.conf with hosts " << joinHosts(hosts, ";") << std::endl;
   std::ofstream fs;
   fs.open ("logcabin.conf", std::fstream::trunc | std::fstream::out);
   fs << "storageModule = filesystem" << std::endl;
   fs << "storagePath = smoketeststorage" << std::endl;
   fs << "servers = " << joinHosts(hosts, ";") << std::endl;
   fs.close();
+  std::cout << "Done writing logcabin.conf" << std::endl;
 }
 
 void
 LogCabinWrapper::bootstrap(int id) {
   std::cout << "Bootstrapping with id " << id << std::endl;
-  system((LOGCABIN_DIR + "build/LogCabin --bootstrap --id " + std::to_string(id) + " &").c_str());
+  system((LOGCABIN_DIR + "build/LogCabin --bootstrap --id " + std::to_string(id) + " > bootstrap.log &").c_str());
   std::cout << "Done boostrapping with id " << id << std::endl;
 }
 
 void
 LogCabinWrapper::startServer(int id) {
   std::cout << "Starting server with id " << id << std::endl;
-  system((LOGCABIN_DIR + "build/LogCabin --id " + std::to_string(id) + " &").c_str());
+  system((LOGCABIN_DIR + "build/LogCabin --id " + std::to_string(id) + " > logcabin_server.log &").c_str());
   std::cout << "Done starting server with id " << id << std::endl;
 }
 
 void
 LogCabinWrapper::reconfigure(const std::vector<std::string>& hosts) {
   std::cout << "Reconfiguring cluster with hosts " << joinHosts(hosts, " ") << std::endl;
-  system((LOGCABIN_DIR + "build/Examples/Reconfigure " + joinHosts(hosts, " ") + " &").c_str());
+  system((LOGCABIN_DIR + "build/Examples/Reconfigure " + joinHosts(hosts, " ") + " > logcabin_reconfigur.log &").c_str());
   std::cout << "Done reconfiguring cluster" << std::endl;
 }
 
