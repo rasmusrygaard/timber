e6fba5a00d427756a49d366fb657b08c3e731a2d
Rasmus Rygaard <rasmus@rasmusrygaard.com>
2014-12-03 19:54:57 -0800

    Downgrade to etcd 4.0

--- etcd/init.sh
+++ etcd/init.sh
@@ -1,5 +1,3 @@
-curl -L  https://github.com/coreos/etcd/releases/download/v0.5.0-alpha.4/etcd-v0.5.0-alpha.4-linux-amd64.tar.gz -o etcd-v0.5.0-alpha.4-linux-amd64.tar.gz
-tar xzvf etcd-v0.5.0-alpha.4-linux-amd64.tar.gz
-export ETCD_HTTP_READ_TIMEOUT=1.0
-export ETCD_HTTP_WRITE_TIMEOUT=1.0
-mv etcd-v0.5.0-alpha.4-linux-amd64 etcd
+curl -L  https://github.com/coreos/etcd/releases/download/v0.4.6/etcd-v0.4.6-linux-amd64.tar.gz -o etcd-v0.4.6-linux-amd64.tar.gz
+tar xzvf etcd-v0.4.6-linux-amd64.tar.gz
+mv etcd-v0.4.6-linux-amd64.tar.gz etcd
