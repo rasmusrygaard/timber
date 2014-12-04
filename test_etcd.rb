4c9908b31a2ef5c6354694aadecf493e44c31880
Rasmus Rygaard <rasmus@rasmusrygaard.com>
2014-12-03 18:24:18 -0800

    Set timeouts in etcd.

--- test_etcd.rb
+++ test_etcd.rb
@@ -4,39 +4,35 @@ require 'set'
 config = open('timber.config').readlines.map(&:rstrip)
 private_ips = config.map { |line| line.split[2] }
 
-clients = private_ips.map { |ip| Etcd.client(host: ip, port: 4001) }
-count = 0
+clients = private_ips.map { |ip| Etcd.client(host: ip, port: 4001, read_timeout: 1) }
 start = Time.now
-client.set('/result', value: [].to_s)
-arr = []
-DURATION = 100 # second
+clients[0].set('/result', value: [0].to_s)
+acked = [0]
+DURATION = 80 # second
 clients.cycle do |client|
   while (Time.now - start) < DURATION
+    res = client.get('/result')
+    arr = JSON.parse(res.value)
+    to_write = arr.last + 1
     begin
-      arr = JSON.parse(client.get('/result').value)
-    rescue Etcd::KeyNotFound
-      arr = []
-      client.set('/result', value: [].to_s)
-    end
-    arr << count
-    begin
-      Timeout::timeout(2) {
-        puts "#{ Time.now } WRITE host=#{ client.host } value=#{ count }"
-        client.set('/result', value: arr.to_s)
-        puts "#{ Time.now } ACK host=#{ client.host } value=#{ count }"
-      }
-    rescue Timeout::Error
-      puts "#{ Time.now } TIMEOUT host=#{ client.host } value=#{ count }"
+      puts "#{ Time.now } WRITE host=#{ client.host } value=#{ to_write }"
+      client.set('/result', value: (arr + [to_write]).to_s)
+      puts "#{ Time.now } ACK host=#{ client.host } value=#{ to_write }"
+      acked << to_write
+    rescue Net::ReadTimeout
+      puts "#{ Time.now } TIMEOUT host=#{ client.host } value=#{ to_write }"
+      break # Break out of the while loop to move to the next client
+    rescue Etcd::TestFailed
+      puts "#{ Time.now } REJECT host=#{ client.host } value=#{ to_write }"
       break # Break out of the while loop to move to the next client
     end
-    count += 1
   end
   break if (Time.now - start) >= DURATION
 end
 
 arr = JSON.parse(client.get('/result').value)
 puts "Elements in result: #{ arr.size }"
-expected_size = arr.max + 1
+expected_size = acked.size
 puts "Expected size: #{ expected_size }"
-puts "Writes Dropped: #{ 100 * (1 - (arr.size / expected_size.to_f)) } %"
+puts "Writes Dropped: #{ 100 * (1 - (arr.uniq.size / expected_size.to_f)) } %"
 puts "Ghost Writes: #{ 100 * ((arr.size - Set.new(arr).size) / expected_size.to_f) } %"
